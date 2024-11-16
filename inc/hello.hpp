745292041571a9ba706343f3e99a0a2a96648b1e
Dmitry Ponyatov <dponyatov@gmail.com>
2024-11-16 21:16:06 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ inc/hello.hpp
@@ -0,0 +1,15 @@
+/// @defgroup lib lib
+/// @{
+/// @defgroup libc libc
+/// @{
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+/// @}
+/// @}
+
+/// @defgroup main main
+/// @{
+int main(int argc, char *argv[]);
+void arg(int argc, char *argv);
+/// @}
--- src/hello.cpp
+++ src/hello.cpp
@@ -1,5 +1,12 @@
 #include "hello.hpp"
 
-int main(int argc, char *argv) {  //
-    argv(0, argv[0]);
+int main(int argc, char *argv[]) {  //
+    arg(0, argv[0]);
+    for (int i = 1; i < argc; i++) {  //
+        arg(i, argv[i]);
+    }
+}
+
+void arg(int argc, char *argv) {  //
+    fprintf(stderr, "arg[%i] = <%s>\n", argc, argv);
 }

390ad8d16007e20821b37e7688f6247392e80952
Dmitry Ponyatov <dponyatov@gmail.com>
2024-11-17 03:15:43 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- inc/hello.hpp
+++ inc/hello.hpp
@@ -10,6 +10,10 @@
 
 /// @defgroup main main
 /// @{
+#ifdef __linux__
 int main(int argc, char *argv[]);
+#else
+void hello(int argc, char *argv[]);
+#endif
 void arg(int argc, char *argv);
 /// @}
--- src/hello.cpp
+++ src/hello.cpp
@@ -1,6 +1,10 @@
 #include "hello.hpp"
 
+#ifdef __linux__
 int main(int argc, char *argv[]) {  //
+#else
+void hello(int argc, char *argv[]) {  //
+#endif
     arg(0, argv[0]);
     for (int i = 1; i < argc; i++) {  //
         arg(i, argv[i]);
